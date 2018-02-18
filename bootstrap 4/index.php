c73541d3944b7a62b73f5554612bc70b8476ff7c
domashkainf <vitalyaotletaesh@mail.ru>
2018-02-18 18:47:25 +0500

    initial commit

---
+++ bootstrap 4/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP and Bootstrap 4 learn</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-expand-sm navbar-dark" style="background:#6C6D69;">
+		<a class="navbar-brand"><img src="img/banana.png" alt="Logo" width="40px" width="30px"> </a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a href="#" class="nav-link ">Home</a>
+				</li>
+				<li class="nav-item ">
+					<a href="#" class="nav-link ">Наши товары</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Контакты
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a href="#" class="dropdown-item">8-982-787-45-11</a>
+						<a href="https://vk.com" target="_blank" class="dropdown-item">Мы в <i class="fab fa-vk" style="font-size: 25px;"></i></a>
+						<a href="https://twitter.com" target="_blank" class="dropdown-item">Мы в <i class="fab fa-twitter" style="font-size: 25px;"></i></a>
+						<a href="https://www.facebook.com" target="_blank" class="dropdown-item">Мы в <i class="fab fa-facebook" style="font-size: 25px;"></i></a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
